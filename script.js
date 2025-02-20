ab0a4d97c90b630daa5b0f19a5b183861559601a
gauravzswal <gauravzswal123@gmail.com>
2025-02-20 15:43:41 +0530

    some changes

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 let div = document.getElementById("div1");
 
-
 div.style.height = "210px";
 div.style.width = "310px";
 div.style.backgroundColor = "yellow";
 div.style.color = "blue";
-div.style.marginTop ="22px"
-div.style.marginLeft ="28px"
-div.style.paddingTop ="10px";
-div.style.paddingLeft ="10px";
-div.style.fontSize ="14px"
-
+div.style.marginTop = "22px";
+div.style.marginLeft = "28px";
+div.style.paddingTop = "10px";
+div.style.paddingLeft = "10px";
+div.style.fontSize = "15px";
