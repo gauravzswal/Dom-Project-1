48174ddc139651e00a144d3176738e5c8aec6396
gauravzswal <gauravzswal123@gmail.com>
2025-02-21 16:19:26 +0530

    inital changes

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 let div = document.getElementById("div1");
 
-div.style.height = "210px";
-div.style.width = "310px";
+div.style.height = "200px";
+div.style.width = "300px";
 div.style.backgroundColor = "yellow";
 div.style.color = "blue";
-div.style.marginTop = "22px";
-div.style.marginLeft = "28px";
-div.style.paddingTop = "10px";
-div.style.paddingLeft = "10px";
-div.style.fontSize = "15px";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize = "18px";
